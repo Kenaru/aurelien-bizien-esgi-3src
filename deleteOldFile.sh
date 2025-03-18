fecdb9846c8e27ef8f2d58dab386231d62bc5186
Kenaru <aurelienbizien@outlook.com>
2025-03-18 16:23:18 +0100

    feat(deletedoldfile): End of work message

--- deleteOldFile.sh
+++ deleteOldFile.sh
@@ -3,6 +3,7 @@ read -p "Path to clean : " path
 if [ -d "$path" ]; then
   find "$path" -type f -mtime +7 -exec rm -f {} \;
   echo "Deleted all files older than 7 days in $path"
+  echo "Congratulation."
 else
   echo "The specified path does not exist."
 fi
