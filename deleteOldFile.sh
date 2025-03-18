8b9c4bb077917b6f98443de0a24e326081baf2f1
Kenaru <aurelienbizien@outlook.com>
2025-03-18 16:30:49 +0100

    fix(deletedoldfile): exit 1 at the end og clean
    Signed-off-by: Kenaru <aurelienbizien@outlook.com>

--- deleteOldFile.sh
+++ deleteOldFile.sh
@@ -3,6 +3,7 @@ read -p "Path to clean : " path
 if [ -d "$path" ]; then
   find "$path" -type f -mtime +7 -exec rm -f {} \;
   echo "Deleted all files older than 7 days in $path"
+    exit 1
 else
   echo "The specified path does not exist."
   echo "Please enter a valid path."
