d934e0d64b093f45e1b185db6c33dcacb09e848e
Kenaru <aurelienbizien@outlook.com>
2025-03-18 16:01:23 +0100

    feat(deleteoldfile): delete file older than 7 days
    Signed-off-by: Kenaru <aurelienbizien@outlook.com>

--- deleteOldFile.sh
+++ deleteOldFile.sh
@@ -1,1 +1,8 @@
 read -p "Path to clean : " path
+
+if [ -d "$path" ]; then
+  find "$path" -type f -mtime +7 -exec rm -f {} \;
+  echo "Deleted all files older than 7 days in $path"
+else
+  echo "The specified path does not exist."
+fi
